344471fb573f0720cada1b8b52a77c4a3304fd13
takahito1970 <83276920+takahito1970@users.noreply.github.com>
2021-06-21 21:56:54 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'taka' && password == '1970'
+    end
+  end
 end
