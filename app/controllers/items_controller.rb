6ca66d07e64eb3245d0929e358abcc1cd8ede9e2
takahito1970 <83276920+takahito1970@users.noreply.github.com>
2021-06-29 11:05:52 +0900

    データベースとモデルの作成〜ログイン、ログアウトのページの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :product_name, :description)
   end
 end
